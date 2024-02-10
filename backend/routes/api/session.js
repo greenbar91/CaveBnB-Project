5cc966b90452bddec5845120c2b4fc3785e39dff
Jacob Dietz <jdietz981@gmail.com>
2024-02-10 15:04:29 -0600

    Add a backend endpoint to get the current user session

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -42,10 +42,31 @@ router.post("/", async (req, res, next) => {
   });
 });
 
+// Restore session user
+router.get(
+    '/',
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        const safeUser = {
+          id: user.id,
+          email: user.email,
+          username: user.username,
+        };
+        return res.json({
+          user: safeUser
+        });
+      } else return res.json({ user: null });
+    }
+  );
+
+
 // Log out
 router.delete("/", (_req, res) => {
   res.clearCookie("token");
   return res.json({ message: "success" });
 });
+
+
 
 module.exports = router;
