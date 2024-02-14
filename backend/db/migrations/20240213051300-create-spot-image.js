1f7360b2887c8f2710508019cbfa25613fba24ba
Jacob Dietz <jdietz981@gmail.com>
2024-02-13 22:57:02 -0600

    added options to all migrations....

--- backend/db/migrations/20240213051300-create-spot-image.js
+++ backend/db/migrations/20240213051300-create-spot-image.js
@@ -1,43 +1,51 @@
-'use strict';
-
+"use strict";
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('SpotImages', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
+    await queryInterface.createTable(
+      "SpotImages",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        url: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        preview: {
+          type: Sequelize.BOOLEAN,
+          allowNull: false,
+        },
+        spotId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: "Spots",
+            key: "id",
+          },
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
       },
-      url: {
-        type: Sequelize.STRING,
-        allowNull:false
-      },
-      preview: {
-        type: Sequelize.BOOLEAN,
-        allowNull:false
-      },
-      spotId: {
-        type: Sequelize.INTEGER,
-        allowNull:false,
-        references:{
-          model: 'Spots',
-          key:'id'
-        }
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-      }
-    });
+      options
+    );
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('SpotImages');
-  }
+    options.tableName = "SpotImages";
+    await queryInterface.dropTable(options);
+  },
 };
--- backend/db/migrations/20240213055106-create-review.js
+++ backend/db/migrations/20240213055106-create-review.js
@@ -1,51 +1,59 @@
-'use strict';
-
+"use strict";
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Reviews', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
+    await queryInterface.createTable(
+      "Reviews",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        userId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: "Users",
+            key: "id",
+          },
+        },
+        spotId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: "Spots",
+            key: "id",
+          },
+        },
+        review: {
+          type: Sequelize.STRING(255),
+          allowNull: false,
+        },
+        stars: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
       },
-      userId: {
-        type: Sequelize.INTEGER,
-        allowNull:false,
-        references:{
-          model:'Users',
-          key:'id'
-        }
-      },
-      spotId: {
-        type: Sequelize.INTEGER,
-        allowNull:false,
-        references:{
-          model:'Spots',
-          key:'id'
-        }
-      },
-      review: {
-        type: Sequelize.STRING(255),
-        allowNull:false
-      },
-      stars: {
-        type: Sequelize.INTEGER,
-        allowNull:false
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-      }
-    });
+      options
+    );
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Reviews');
-  }
+    options.tableName = "Reviews";
+    await queryInterface.dropTable(options);
+  },
 };
