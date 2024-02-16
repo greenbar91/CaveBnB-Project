2937a7aa6d4e9d2334b16c6a02e02967fd1658cd
Jacob Dietz <jdietz981@gmail.com>
2024-02-15 23:56:37 -0600

    Added Create a Spot

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -5,9 +5,13 @@ module.exports = (sequelize, DataTypes) => {
   class Spot extends Model {
     static associate(models) {
       Spot.belongsTo(models.User, {
-        foreignKey: "ownerId", as:'Owner'
+        foreignKey: "ownerId",
+        as: "Owner",
       });
-      Spot.hasMany(models.SpotImage, { foreignKey: "spotId" ,as: "SpotImages"});
+      Spot.hasMany(models.SpotImage, {
+        foreignKey: "spotId",
+        as: "SpotImages",
+      });
       Spot.hasMany(models.Review, { foreignKey: "spotId" });
       Spot.hasMany(models.Booking, { foreignKey: "spotId" });
     }
@@ -19,19 +23,37 @@ module.exports = (sequelize, DataTypes) => {
       city: { type: DataTypes.STRING, allowNull: false },
       state: { type: DataTypes.STRING, allowNull: false },
       country: { type: DataTypes.STRING, allowNull: false },
-      lat: { type: DataTypes.DECIMAL, allowNull: false,
-      validate:{
-        min:-90,
-        max:90
-      } },
-      lng: { type: DataTypes.DECIMAL, allowNull: false ,
-      validate:{
-        min:-180,
-        max:180
-      }},
-      name: { type: DataTypes.STRING, allowNull: false },
+      lat: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+        validate: {
+          min: -90,
+          max: 90,
+        },
+      },
+      lng: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+        validate: {
+          min: -180,
+          max: 180,
+        },
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [2, 49]
+        },
+      },
       description: { type: DataTypes.STRING, allowNull: false },
-      price: { type: DataTypes.INTEGER, allowNull: false },
+      price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          min: 1,
+        },
+      },
     },
     {
       sequelize,
