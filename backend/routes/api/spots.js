f1cb56d4d69697c3bb799c4ea724e94b166598ac
Jacob Dietz <jdietz981@gmail.com>
2024-02-15 22:53:17 -0600

    Added Get all Spots owned by the Current User

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -12,5 +12,16 @@ router.get('/', async (req,res) => {
     return res.status(200).json(allSpots)
 })
 
+//Get all Spots owned by the Current User
+router.get('/current', async (req,res) => {
+    const currentUserSpots = await Spot.findAll({
+        where:{
+            ownerId: req.user.id
+        }
+    })
+
+    return res.status(200).json(currentUserSpots)
+})
+
 
 module.exports = router;
