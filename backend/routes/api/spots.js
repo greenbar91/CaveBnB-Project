4b0c27be21a60f13edfaa8f4ca3a350c23ff37dc
Jacob Dietz <jdietz981@gmail.com>
2024-02-15 21:27:51 -0600

    fixed options for migrations

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,3 +1,18 @@
 const express = require("express");
-const router = express.Router();
 const {Spot} = require('../db/models')
+
+
+const router = express.Router();
+
+//Get all the spots
+router.get('/', async (req,res) => {
+
+    const allSpots = await Spot.findAll()
+
+    console.log(allSpots)
+
+    return res.status(200).json(allSpots)
+})
+
+
+module.exports = router;
