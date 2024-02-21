163ee893cd4bacf5378e00c22b512b23ef3a6a22
Jacob Dietz <jdietz981@gmail.com>
2024-02-20 20:51:10 -0600

    Fixed delete cascade

--- backend/db/migrations/20240213055106-create-review.js
+++ backend/db/migrations/20240213055106-create-review.js
@@ -29,7 +29,8 @@ module.exports = {
           references: {
             model: "Spots",
             key: "id",
-          }
+          },
+          onDelete:'CASCADE'
         },
         review: {
           type: Sequelize.STRING(255),
