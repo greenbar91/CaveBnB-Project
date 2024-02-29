42d00b1f7375239af1d57a55d5301e18dfcc8ac8
Jacob Dietz <jdietz981@gmail.com>
2024-02-29 14:52:11 -0600

    Cleaned up 403/404 status placement and added comment title to review-images

--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -4,31 +4,43 @@ const { requireAuth } = require("../../utils/auth");
 
 const router = express.Router();
 
-//Delete a Review Image
-router.delete("/:imageId", requireAuth, async (req, res) => {
-  const { imageId } = req.params;
+//--------------------------------------------------------------------------------------//
+//                                Delete a Review Image                                 //
+//--------------------------------------------------------------------------------------//
+router.delete(
+  "/:imageId",
+  requireAuth,
+  async (req, res, next) => {
+    const { imageId } = req.params;
 
-  const findImageById = await ReviewImage.findByPk(imageId);
+    const findImageById = await ReviewImage.findByPk(imageId);
 
-  if (!findImageById) {
-    return res.status(404).json({
-      message: "Review Image couldn't be found",
+    if (!findImageById) {
+      return res.status(404).json({
+        message: "Review Image couldn't be found",
+      });
+    }
+
+    const findReviewByReviewId = await Review.findByPk(findImageById.reviewId);
+
+    if (findReviewByReviewId.userId !== req.user.id) {
+      return res.status(403).json({
+        message: "Forbidden",
+      });
+    }
+    next();
+  },
+  async (req, res) => {
+    const { imageId } = req.params;
+
+    const findImageById = await ReviewImage.findByPk(imageId);
+
+    await findImageById.destroy();
+
+    return res.status(200).json({
+      message: "Successfully deleted",
     });
   }
-
-  const findReviewByReviewId = await Review.findByPk(findImageById.reviewId);
-
-  if (findReviewByReviewId.userId !== req.user.id) {
-    return res.status(403).json({
-      message: "Forbidden",
-    });
-  }
-
-  await findImageById.destroy();
-
-  return res.status(200).json({
-    message: "Successfully deleted",
-  });
-});
+);
 
 module.exports = router;
