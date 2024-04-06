dce28d490dc6d78b8206cc004086ca1378b69b05
Jacob Dietz <jdietz981@gmail.com>
2024-04-06 13:40:56 -0500

    Added toggle for clicking on and outside the profile button

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,22 +1,47 @@
-
+import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { FaUserCircle } from 'react-icons/fa';
 import * as sessionActions from '../../store/session';
+import '../Navigation/Navigation.css'
+
+
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
+
+
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = () => {
+      setShowMenu(false);
+    };
+
+    document.addEventListener('click', closeMenu);
+
+    return () => document.removeEventListener('click', closeMenu);
+  }, [showMenu]);
+
+  const toggleMenu = (e) => {
+    e.stopPropagation(); // Keep click from bubbling up to document and triggering closeMenu
+    setShowMenu(!showMenu);
+  };
+
 
   const logout = (e) => {
     e.preventDefault();
     dispatch(sessionActions.logoutThunk());
   };
 
+  const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+
   return (
     <>
-      <button>
+       <button onClick={toggleMenu}>
         <FaUserCircle />
       </button>
-      <ul className="profile-dropdown">
+      <ul className={ulClassName}>
         <li>{user.username}</li>
         <li>{user.firstName} {user.lastName}</li>
         <li>{user.email}</li>
