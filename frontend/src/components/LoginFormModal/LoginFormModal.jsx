b30cdbd7e111a77e6f296fd63bfef19ff12ef608
Jacob Dietz <jdietz981@gmail.com>
2024-04-06 16:10:16 -0500

    added styling

--- frontend/src/components/LoginFormModal/LoginFormModal.jsx
+++ frontend/src/components/LoginFormModal/LoginFormModal.jsx
@@ -26,7 +26,7 @@ function LoginFormModal() {
 
   return (
     <>
-      <h1>Log In</h1>
+      <h1 className='login'>Log In</h1>
       <form onSubmit={handleSubmit}>
         <label>
           Username or Email
