5404515a75c84c309c1367cb53c6d9ed817fab2d
Jacob Dietz <jdietz981@gmail.com>
2024-04-14 17:31:40 -0500

    fixed order display on homepage, pushing dist to main for deploy testing

--- frontend/src/components/HomePage/HomePage.jsx
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import * as spotsActions from "../../store/spots";
 import "./HomePage.css";
 import { Link } from "react-router-dom";
@@ -8,42 +8,50 @@ import { FaStar } from "react-icons/fa";
 export default function HomePage() {
   const dispatch = useDispatch();
   const spots = useSelector((state) => state.spots.Spots);
+  const [sortedSpots, setSortedSpots] = useState(null);
 
   useEffect(() => {
     dispatch(spotsActions.getAllSpotsThunk());
   }, [dispatch]);
 
-  if (!spots) {
+  useEffect(() => {
+    if (spots) {
+      const sorted = [...spots].sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+      setSortedSpots(sorted);
+    }
+  }, [spots]);
+
+  if (!sortedSpots) {
     return null;
   }
 
   return (
     <ul className="spots-container">
-      {Object.keys(spots).map((spotId) => {
-        const spot = spots[spotId];
-        // console.log(spot)
+      {Object.keys(sortedSpots).map((spotId) => {
+        const spot = sortedSpots[spotId];
+
         return (
           <>
             <div key={spotId} className="spot">
-              <Link to={`spots/${spot.id}`}>
+              <Link to={`spots/${spot?.id}`}>
                 <div className="image-container">
-                  <img src={spot.previewImage}></img>
+                  <img src={spot?.previewImage}></img>
                 </div>
                 <div className="spot-info-container">
                   <div className="spot-info-left">
-                    <li title={spot.name}>
-                      {spot.city}, {spot.state}
+                    <li title={spot?.name}>
+                      {spot?.city}, {spot?.state}
                     </li>
                     <li className="price">
-                      ${spot.price} <span className="night">night</span>
+                      ${spot?.price} <span className="night">night</span>
                     </li>
                   </div>
                   <div className="spot-info-right">
                     <li>
-                      {spot.avgRating ? (
+                      {spot?.avgRating ? (
                         <>
                           <FaStar />{" "}
-                          {spot.avgRating.toFixed(1)}
+                          {spot?.avgRating.toFixed(1)}
                         </>
                       ) : (
                         "New"
