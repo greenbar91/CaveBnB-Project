7578c51fe21023200ea6e52451184a38cd578df7
Jacob Dietz <jdietz981@gmail.com>
2024-04-14 16:16:13 -0500

    pushing for debugging

--- frontend/src/components/CreateSpotsPage/CreateSpotPage.jsx
+++ frontend/src/components/CreateSpotsPage/CreateSpotPage.jsx
@@ -1,36 +1,147 @@
 import "./CreateSpotPage.css";
 import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import * as sessionActions from "../../store/session";
 import * as spotActions from "../../store/spots";
 
 export default function CreateSpotPage() {
   const dispatch = useDispatch();
+  const ownerId = useSelector((state) => state.session.user.id);
   const [address, setAddress] = useState("");
   const [city, setCity] = useState("");
   const [state, setState] = useState("");
   const [country, setCountry] = useState("");
-  const [lat, setLat] = useState(0);
-  const [lng, setLng] = useState(0);
+  const [lat, setLat] = useState();
+  const [lng, setLng] = useState();
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
-  const [price, setPrice] = useState(0);
-  const [errors,setErrors] = useState({})
+  const [price, setPrice] = useState();
+  const [errors, setErrors] = useState({});
+  const [previewUrl, setPreviewUrl] = useState("");
+  const [url1, setUrl1] = useState("");
+  const [url2, setUrl2] = useState("");
+  const [url3, setUrl3] = useState("");
+  const [url4, setUrl4] = useState("");
 
-  const handleSubmit = (e) => {
+  const HandleSubmit = async (e) => {
     e.preventDefault();
-    setErrors({})
+    setErrors({});
 
-    const checkErrors = {}
+    const checkErrors = {};
 
-    if()
+    if (!address.length) {
+      checkErrors.address = "Address is required";
+    }
+    if (!city.length) {
+      checkErrors.city = "City is required";
+    }
+    if (!state.length) {
+      checkErrors.state = "State is required";
+    }
+    if (!country.length) {
+      checkErrors.country = "Country is required";
+    }
+    if (!lat) {
+      checkErrors.lat = "Latitude is required";
+    }
+    if (!lng) {
+      checkErrors.lng = "Longitude is required";
+    }
+    if (!name.length) {
+      checkErrors.name = "Name is required";
+    }
+    if (!price) {
+      checkErrors.price = "Price is required";
+    }
+    if (!previewUrl.length) {
+      checkErrors.previewUrl = "Preview image is required";
+    }
 
-  }
+    if (description.length < 30) {
+      checkErrors.description = "Description needs a minimum of 30 characters";
+    }
 
-  let error = true;
+    const validateImageUrl = (url) => {
+      const pattern = /\.(png|jpe?g)$/i;
+      return pattern.test(url);
+    };
+
+    if (!validateImageUrl(url1) && url1.length) {
+      checkErrors.url1 =
+        "Please enter a valid image URL ending with .png, .jpg, or .jpeg";
+    }
+    if (!validateImageUrl(url2)&& url2.length) {
+      checkErrors.url2 =
+        "Please enter a valid image URL ending with .png, .jpg, or .jpeg";
+    }
+    if (!validateImageUrl(url3)&& url3.length) {
+      checkErrors.url3 =
+        "Please enter a valid image URL ending with .png, .jpg, or .jpeg";
+    }
+    if (!validateImageUrl(url4) && url4.length) {
+      checkErrors.url4 =
+        "Please enter a valid image URL ending with .png, .jpg, or .jpeg";
+    }
+
+    if (!Object.values(checkErrors).length) {
+      const newSpot = {
+        ownerId,
+        address,
+        city,
+        state,
+        country,
+        lat,
+        lng,
+        name,
+        description,
+        price,
+      };
+
+      const createdSpot = await dispatch(spotActions.createSpotThunk(newSpot));
+      const createdSpotId = createdSpot.id;
+
+      const newSpotPreviewImage = {
+        url: previewUrl,
+        preview: true,
+        spotId: createdSpotId,
+      };
+
+      const newSpotImage1 = {
+        url: url1,
+        preview: false,
+        spotId: createdSpotId,
+      };
+
+      const newSpotImage2 = {
+        url: url2,
+        preview: false,
+        spotId: createdSpotId,
+      };
+
+      const newSpotImage3 = {
+        url: url3,
+        preview: false,
+        spotId: createdSpotId,
+      };
+
+      const newSpotImage4 = {
+        url: url4,
+        preview: false,
+        spotId: createdSpotId,
+      };
+
+      dispatch(spotActions.createSpotImageThunk(newSpotPreviewImage))
+
+
+    }
+
+    setErrors(checkErrors);
+  };
+
+
   return (
     <div className="create-spot-container">
-      <form className="create-spot-form">
+      <form className="create-spot-form" onSubmit={HandleSubmit}>
         <section className="create-spot-form-container">
           <h1>Create a new Spot</h1>
           <h3>Where&apos;s your place located?</h3>
@@ -39,38 +150,62 @@ export default function CreateSpotPage() {
             reservation.
           </p>
           <div className="input-wrapper">
-            <label htmlFor="" className="country" >
-              Country {error && <span className="error-msg">* error msg</span>}
+            <label htmlFor="" className="country">
+              Country {errors && <span className="error-msg"> {errors.country}</span>}
             </label>
-            <input placeholder="Country" value={country} onChange={(e)=> setCountry(e.target.value)} />
+            <input
+              placeholder="Country"
+              value={country}
+              onChange={(e) => setCountry(e.target.value)}
+            />
             <label>
               Street Address{" "}
-              {error && <span className="error-msg">* error msg</span>}
+              {errors && <span className="error-msg"> {errors.address}</span>}
             </label>
-            <input placeholder="Street Address" />
+            <input
+              placeholder="Street Address"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+            />
             <label>
-              City {error && <span className="error-msg">* error msg</span>}
+              City {errors && <span className="error-msg"> {errors.city}</span>}
             </label>
-            <input placeholder="City" />
+            <input
+              placeholder="City"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+            />
             <label>
-              State {error && <span className="error-msg">* error msg</span>}
+              State {errors && <span className="error-msg"> {errors.state}</span>}
             </label>
-            <input placeholder="STATE" />
+            <input
+              placeholder="STATE"
+              value={state}
+              onChange={(e) => setState(e.target.value)}
+            />
             <label>
-              Latitude {error && <span className="error-msg">* error msg</span>}
+              Latitude {errors && <span className="error-msg"> {errors.lat}</span>}
             </label>
-            <input placeholder="Latitude" />
+            <input
+              placeholder="Latitude"
+              value={lat}
+              onChange={(e) => setLat(e.target.value)}
+            />
             <label>
               Longitude{" "}
-              {error && <span className="error-msg">* error msg</span>}
+              {errors && <span className="error-msg"> {errors.lng}</span>}
             </label>
-            <input placeholder="Longitude" />
+            <input
+              placeholder="Longitude"
+              value={lng}
+              onChange={(e) => setLng(e.target.value)}
+            />
           </div>
         </section>
         <section className="create-spot-form-container">
           <h1>Describe your place to guests</h1>
           <p>
-            Mention the best features of your space, any special amentities like
+            Mention the best features of your space, any special amenities like
             fast wif or parking, and what you love about the neighborhood.
           </p>
           <div className="input-wrapper">
@@ -78,8 +213,10 @@ export default function CreateSpotPage() {
               <textarea
                 placeholder="Description"
                 className="description-input"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
               />
-              {error && <span className="error-msg">* error msg</span>}
+              {errors && <span className="error-msg"> {errors.description}</span>}
             </label>
           </div>
         </section>
@@ -91,8 +228,12 @@ export default function CreateSpotPage() {
           </p>
           <div className="input-wrapper">
             <label></label>
-            <input placeholder="Name of your spot" />
-            {error && <span className="error-msg">* error msg</span>}
+            <input
+              placeholder="Name of your spot"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            {errors && <span className="error-msg"> {errors.name}</span>}
           </div>
         </section>
         <section className="create-spot-form-container">
@@ -103,9 +244,14 @@ export default function CreateSpotPage() {
           </p>
           <div className="input-wrapper">
             <label>
-              $ <input placeholder="Price per Night (USD)" />
+              ${" "}
+              <input
+                placeholder="Price per Night (USD)"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+              />
             </label>
-            {error && <span className="error-msg">* error msg</span>}
+            {errors && <span className="error-msg"> {errors.price}</span>}
           </div>
         </section>
         <section className="create-spot-form-container">
@@ -113,28 +259,48 @@ export default function CreateSpotPage() {
           <p>Submit a link to at least one photo to publish your spot.</p>
           <div className="input-wrapper">
             <label>
-              <input placeholder="Preview Image URL" />
+              <input
+                placeholder="Preview Image URL"
+                value={previewUrl}
+                onChange={(e) => setPreviewUrl(e.target.value)}
+              />
             </label>
-            {error && <span className="error-msg">* error msg</span>}
+            {errors && <span className="error-msg"> {errors.previewUrl}</span>}
             <label>
-              <input placeholder="Image URL" />
+              <input
+                placeholder="Image URL"
+                value={url1}
+                onChange={(e) => setUrl1(e.target.value)}
+              />
             </label>
-            {error && <span className="error-msg">* error msg</span>}
+            {errors && <span className="error-msg"> {errors.url1}</span>}
             <label>
-              <input placeholder="Image URL" />
+              <input
+                placeholder="Image URL"
+                value={url2}
+                onChange={(e) => setUrl2(e.target.value)}
+              />
             </label>
-            {error && <span className="error-msg">* error msg</span>}
+            {errors && <span className="error-msg"> {errors.url2}</span>}
             <label>
-              <input placeholder="Image URL" />
+              <input
+                placeholder="Image URL"
+                value={url3}
+                onChange={(e) => setUrl3(e.target.value)}
+              />
             </label>
-            {error && <span className="error-msg">* error msg</span>}
+            {errors && <span className="error-msg"> {errors.url3}</span>}
             <label>
-              <input placeholder="Image URL" />
+              <input
+                placeholder="Image URL"
+                value={url4}
+                onChange={(e) => setUrl4(e.target.value)}
+              />
             </label>
-            {error && <span className="error-msg">* error msg</span>}
+            {errors && <span className="error-msg">{errors.url4}</span>}
           </div>
         </section>
-        <button className="create-spot-submit-button">Create Spot</button>
+        <button className="create-spot-submit-button" type="submit">Create Spot</button>
       </form>
     </div>
   );
--- frontend/src/store/spots.js
+++ frontend/src/store/spots.js
@@ -2,16 +2,23 @@ import { csrfFetch } from "./csrf";
 
 const GET_SPOTS = "spots/getSpots";
 const GET_SPOT_DETAILS = "spots/getSpotDetails";
+const CREATE_SPOT = "spots/createSpot";
+const CREATE_SPOT_IMAGE = "spots/createSpotImage";
 
 export const getSpots = (payload) => {
-  return {
-    type: GET_SPOTS,
-    payload,
-  };
+  return { type: GET_SPOTS, payload };
 };
 
 export const getSpotDetails = (payload) => {
   return { type: GET_SPOT_DETAILS, payload };
+};
+
+export const createSpot = (payload) => {
+  return { type: CREATE_SPOT, payload };
+};
+
+export const createSpotImage = (payload) => {
+  return { type: CREATE_SPOT_IMAGE, payload };
 };
 
 export const getAllSpotsThunk = () => async (dispatch) => {
@@ -28,27 +35,81 @@ export const getAllSpotsThunk = () => async (dispatch) => {
 };
 
 export const getSpotDetailsThunk = (spotId) => async (dispatch) => {
-    const res = await csrfFetch(`/api/spots/${spotId}`)
+  const res = await csrfFetch(`/api/spots/${spotId}`);
 
-    if(res.ok){
-        const data = await res.json();
-        dispatch(getSpotDetails(data));
-        return data
-    }
-    if(!res.ok){
-        const data = await res.json()
-        return data
-    }
+  if (res.ok) {
+    const data = await res.json();
+    dispatch(getSpotDetails(data));
+    return data;
+  }
+  if (!res.ok) {
+    const data = await res.json();
+    return data;
+  }
+};
+
+export const createSpotThunk = (newSpot) => async (dispatch) => {
+  const res = await csrfFetch("/api/spots", {
+    method: "POST",
+    header: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newSpot),
+  });
+
+  if (res.ok) {
+    const data = await res.json();
+    dispatch(createSpot(data));
+    return data;
+  }
+  if (!res.ok) {
+    const data = await res.json();
+    return data;
+  }
+};
+
+export const createSpotImageThunk = (image) => async (dispatch) => {
+  const res = await csrfFetch(`/api/spots/${image.spotId}/images`,{
+    method:'POST',
+    header: {
+      "Content-Type": "application/json",
+    },
+    body:JSON.stringify(image)
+  })
+
+  if(res.ok){
+    const data = await res.json()
+    dispatch(createSpotImage(data))
+    return data
+  }
+
+  if (!res.ok) {
+    const data = await res.json();
+    return data;
+  }
 }
 
-const initialState ={} ;
+const initialState = {};
 
 const spotsReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_SPOTS:
       return { ...state, ...action.payload };
     case GET_SPOT_DETAILS:
-        return {...state, [action.payload.id]:action.payload}
+      return { ...state, [action.payload.id]: action.payload };
+    case CREATE_SPOT:
+      return { ...state, [action.payload.id]: action.payload };
+    case CREATE_SPOT_IMAGE:
+      return {
+        ...state,
+        [action.payload.spotId]: {
+          ...state[action.payload.spotId],
+          SpotImages: [
+            ...state[action.payload.spotId].SpotImages,
+            { url: action.payload.url, preview: action.payload.preview , spotId:action.payload.spotId},
+          ],
+        },
+      };
     default:
       return state;
   }
